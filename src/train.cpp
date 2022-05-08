af1043c1e118aac0f51e15044626fb8abe7c9b87
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-05-08 22:07:56 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -2,39 +2,38 @@
 #include "train.h"
 
 void Train::addCage(bool light) {
- if (!first) {
+  if (!first) {
   first = create(true);
- }
- else if (first->prev == nullptr) {
-  first->next = create(light);
-  first->next->prev = first;
-  first->next->next = first;
-  first->prev = first->next;
- }
- else {
-  first->prev->next = create(light);
-  first->prev->next->next = first;
-  first->prev->next->prev = first->prev;
-  first->prev = first->prev->next;
- }
+  } else if (first->prev == nullptr) {
+   
+   first->next = create(light);
+   first->next->prev = first;
+   first->next->next = first;
+   first->prev = first->next;
+  } else {
+   first->prev->next = create(light);
+   first->prev->next->next = first;
+   first->prev->next->prev = first->prev;
+   first->prev = first->prev->next;
+  }
 }
 
 int Train::getLenght() {
- now = first;
- while (first->light) {
-  shagi = 0;
-  do {
-   shagi += 1;
-   countOp += 1;
-   now = now->next;
-  } while (!now->light);
-  now->light = false;
-  countOp += shagi;
   now = first;
- }
- return shagi;
+  while (first->light) {
+   shagi = 0;
+   do {
+    shagi += 1;
+    countOp += 1;
+    now = now->next;
+   } while (!now->light);
+    now->light = false;
+    countOp += shagi;
+    now = first;
+   }
+  return shagi;
 }
 
 int Train::getOpCount() {
- return countOp;
+  return countOp;
 }
